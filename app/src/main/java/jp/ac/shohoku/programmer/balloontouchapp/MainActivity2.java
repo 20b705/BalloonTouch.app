f38c28ff09ad00fc2fa843292b0b1c6b6314c431
20B707 <s20b707@shohoku.ac.jp>
2021-01-22 11:00:41 +0900

    サクヤ

--- app/src/main/java/jp/ac/shohoku/programmer/balloontouchapp/MainActivity2.java
+++ app/src/main/java/jp/ac/shohoku/programmer/balloontouchapp/MainActivity2.java
@@ -1,13 +1,10 @@
 package jp.ac.shohoku.programmer.balloontouchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.CountDownTimer;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
-
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
@@ -23,59 +20,31 @@ public class MainActivity2 extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
 
-        // 10秒= 10x1000 = 10000 msec
         long countNumber = 10000;
-        // インターバル msec
         long interval = 10;
 
-        Button startButton = findViewById(R.id.start_button);
-        Button stopButton = findViewById(R.id.stop_button);
 
-        timerText = findViewById(R.id.timer);
+        timerText = findViewById(R.id.Timer);
         timerText.setText(dataFormat.format(0));
 
-        // インスタンス生成
-        // CountDownTimer(long millisInFuture, long countDownInterval)
         final CountDown countDown = new CountDown(countNumber, interval);
 
-        startButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // 開始
-                countDown.start();
-            }
-        });
 
-        stopButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                // 中止
-                countDown.cancel();
-                timerText.setText(dataFormat.format(0));
-            }
-        });
     }
     class CountDown extends CountDownTimer {
-
         CountDown(long millisInFuture, long countDownInterval) {
             super(millisInFuture, countDownInterval);
         }
 
         @Override
         public void onFinish() {
-            // 完了
+            Intent intent = new Intent(MainActivity2.this, MainActivity3.class);
+            startActivity(intent);
             timerText.setText(dataFormat.format(0));
         }
 
-        // インターバルで呼ばれる
         @Override
         public void onTick(long millisUntilFinished) {
-            // 残り時間を分、秒、ミリ秒に分割
-            //long mm = millisUntilFinished / 1000 / 60;
-            //long ss = millisUntilFinished / 1000 % 60;
-            //long ms = millisUntilFinished - ss * 1000 - mm * 1000 * 60;
-            //timerText.setText(String.format("%1$02d:%2$02d.%3$03d", mm, ss, ms));
-
             timerText.setText(dataFormat.format(millisUntilFinished));
 
         }
