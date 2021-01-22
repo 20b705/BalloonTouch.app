59002387232947335a32be8abe75a6c5232ded0c
20B707 <s20b707@shohoku.ac.jp>
2021-01-22 11:47:38 +0900

    プリン

--- app/src/main/java/jp/ac/shohoku/programmer/balloontouchapp/MainActivity3.java
+++ app/src/main/java/jp/ac/shohoku/programmer/balloontouchapp/MainActivity3.java
@@ -2,7 +2,10 @@ package jp.ac.shohoku.programmer.balloontouchapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +13,14 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button nextButton = findViewById(R.id.return_button);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
